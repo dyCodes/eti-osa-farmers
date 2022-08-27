a99c6a29ab6eb8ea55ce86b48f756c96cbcfaff5
dyCodes <yusufdaudu51@gmail.com>
2022-08-27 16:33:51 +0100

    Add modal pop-up

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,38 +1,70 @@
 // 
-const myEvents = [
-  {
-    start: '2022-08-10T06:00:00',
-    end: '2022-08-10T20:30:00',
-    name: 'Event 1',
-  },
-  // {
-  //   start: '2022-08-22T06:00:00',
-  //   end: '2022-08-22T20:30:00',
-  //   name: 'Event 2',
-  // }, {
-  //   start: '2022-08-19T06:00:00',
-  //   end: '2022-08-19T20:30:00',
-  //   name: 'Event 2',
-  // },
-];
 
 $(function () {
-  new Calendar({
-    id: '#color-calendar',
-    eventsData: myEvents,
-    dateChanged: (currentDate, DateEvents) => {
-      // do something
-      console.log(currentDate, DateEvents);
-    },
-  })
+  // Modal steps
+  $('.modal-footer .modalBtn').click(function (e) {
+    // e.preventDefault();
+    let nextEl = '#' + $(this).data('next');
+    console.log(nextEl);
+    $('#collateralModal .modal-content').addClass('d-none');
+    $(nextEl).removeClass('d-none');
+  });
 
-  // $('.calendar-container').calendar({
-  //   date: new Date(),
-  //   weekDayLength: 3,
-  //   showTodayButton: false,
-  //   highlightSelectedWeek: false,
-  //   onClickDate: function (date) {
-  //     console.log(date);
-  //   },
-  // });
+  // Reset modal to step 1
+  $('.modal-footer .successModalBtn').click(function (e) {
+    setTimeout(() => {
+      let nextEl = '#' + $(this).data('next');
+      $('#collateralModal .modal-content').addClass('d-none');
+      $(nextEl).removeClass('d-none');
+    }, 1000);
+  });
+
+  // OTP
+  $('.digit-group').find('input').each(function () {
+    $(this).attr('maxlength', 1);
+    $(this).on('keyup', function (e) {
+      var parent = $($(this).parent());
+
+      if (e.keyCode === 8 || e.keyCode === 37) {
+        var prev = parent.find('input#' + $(this).data('previous'));
+
+        if (prev.length) {
+          $(prev).select();
+        }
+      } else if ((e.keyCode >= 48 && e.keyCode <= 57) || (e.keyCode >= 65 && e.keyCode <= 90) || (e.keyCode >= 96 && e.keyCode <= 105) || e.keyCode === 39) {
+        var next = parent.find('input#' + $(this).data('next'));
+
+        if (next.length) {
+          $(next).select();
+        } else {
+          if (parent.data('autosubmit')) {
+            parent.submit();
+          }
+        }
+      }
+    });
+  });
+
+  // Calendar
+  const myEvents = [
+    {
+      start: '2022-08-10T06:00:00',
+      end: '2022-08-10T20:30:00',
+      name: 'Event 1',
+    },
+  ];
+
+  try {
+    new Calendar({
+      id: '#color-calendar',
+      eventsData: myEvents,
+      dateChanged: (currentDate, DateEvents) => {
+        // do something
+        console.log(currentDate, DateEvents);
+      },
+    })
+  } catch (e) {
+    console.log(e);
+  }
+
 });
